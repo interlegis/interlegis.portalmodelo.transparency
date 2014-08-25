866dd650aeb74f0bd979bcb17588e670c6d13330
Érico Andrei <erico@simplesconsultoria.com.br>
2014-08-25 09:46:13 -0300

    Back to development: y

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages
 from setuptools import setup
 
-version = '1.0b2'
+version = 'y.dev0'
 description = 'Portal Modelo: Dados Tabulares'
 long_description = (
     open('README.rst').read() + '\n' +
